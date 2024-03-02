daa7e873c4b9682b99a67b78b52c1889f24bb6a3
garima.9613 <garima.sharma9613@gmail.com>
2024-03-02 13:29:30 +0530

    some changes

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -54,7 +54,11 @@ export default function List() {
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
           <input
             type="text"
-            onChange={(e) => setSearchFilter(e.target.value)}
+            onChange={(e) =>
+              setSearchFilter(
+                e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
+              )
+            }
             className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
             placeholder="Search by Country Code (ex. AD,Br,AG etc)"
             aria-label="Search"
